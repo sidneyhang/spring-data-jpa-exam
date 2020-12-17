70feda4bbd948b87c79ab396feadbd8ab6871975
sidney <sidney.hang.y@gmail.com>
2020-12-17 14:07:34 +0800

    init version

---
+++ src/main/java/com/example/jpa/exam/SpringDataJpaExamApplication.java
@@ -0,0 +1,16 @@
+package com.example.jpa.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sidney
+ */
+@SpringBootApplication
+public class SpringDataJpaExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpa/exam/SpringDataJpaExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
